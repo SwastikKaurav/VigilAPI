8012fb88690ef9eb8abab430d473115a8e734406
SwastikKaurav <kauravswastik15@gmail.com>
2026-08-30 23:18:37 +0530

    Added some crud functions

---
+++ crud.py
@@ -0,0 +1,7 @@
+import database_models
+from database import SessionLocal
+from sqlalchemy.orm import Session
+
+def get_apis(db : Session){
+    
+}
--- models.py
+++ models.py
@@ -4,16 +4,17 @@ from datetime import datetime
 class EndpointInput(BaseModel):
     name:str
     url:str
+    ping_interval:int = 300
 
 class EndpointResponse(BaseModel):
-    model_config = {"from_attributes", True}
+    model_config = {"from_attributes": True}
     id:int
     name:str
     url:str
     ping_interval:int
 
 class PingResultResponse(BaseModel):
-    model_config = {"from_attributes", True}
+    model_config = {"from_attributes": True}
     id:int
     endpoint_id:int
     status_code:int
