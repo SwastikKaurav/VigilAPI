d965849c2230d65f854e1b62855bf1b2cdddefc4
SwastikKaurav <kauravswastik15@gmail.com>
2026-08-31 16:02:27 +0530

    get request route done

---
+++ main.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI, Depends
+from crud import get_endpoint, get_all_endpoints, create_endpoint, update_endpoint, delete_endpoint, create_ping_result
+from database import get_db
+from sqlalchemy.orm import Session
+from models import EndpointResponse, PingResultResponse
+
+app = FastAPI()
+
+@app.get("/",response_model = EndpointResponse)
+def get_endpoints(db : Session = Depends(get_db)):
+    return get_all_endpoints(db)
