e1842b51defa3ef7e58510e2dc52c8908f08589e
SwastikKaurav <kauravswastik15@gmail.com>
2026-08-31 16:12:52 +0530

    create function added

--- main.py
+++ main.py
@@ -2,15 +2,19 @@ from fastapi import FastAPI, Depends
 from crud import get_endpoint, get_all_endpoints, create_endpoint, update_endpoint, delete_endpoint, create_ping_result
 from database import get_db
 from sqlalchemy.orm import Session
-from models import EndpointResponse, PingResultResponse
+from models import EndpointInput, EndpointResponse, PingResultResponse
+from database_models import Endpoint, PingResult
 
 app = FastAPI()
 
 @app.get("/", response_model = EndpointResponse)
-def get_endpoints(db : Session = Depends(get_db)):
+def getAllEndpoint(db : Session = Depends(get_db)):
     return get_all_endpoints(db)
 
-@app.get("/{endpoint_id}", response_model = EndpointResponse)
-def get_endpoint(endpoint_id : int, db : Session = Depends(get_db)):
+@app.get("/endpoint/{endpoint_id}", response_model = EndpointResponse)
+def getEndpoint(endpoint_id : int, db : Session = Depends(get_db)):
     return get_endpoint(db, endpoint_id)
 
+@app.post("/endpoint/create", response_model = EndpointResponse)
+def createEndpoint(endpoint_input : EndpointInput, db : Session = Depends(get_db)):
+    return create_endpoint(endpoint_input, db)
