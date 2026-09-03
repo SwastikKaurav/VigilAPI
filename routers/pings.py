d49ff3d6f456f6ba2d26e0635a59ea58edb3cf6f
SwastikKaurav <kauravswastik15@gmail.com>
2026-09-03 12:24:02 +0530

    added pings.py and added function of it in crud.py

--- routers/pings.py
+++ routers/pings.py
@@ -5,8 +5,8 @@ from sqlalchemy.orm import Session
 from database import get_db
 from models import PingResultResponse
 
-router = APIRouter(prefix="/endpoints", tags=["endpoints"])
+router = APIRouter(prefix="/endpoints", tags=["pings"])
 
-@router.get("/endpoints/{endpoint_id}/pings", response_model=list[PingResultResponse])
+@router.get("/{endpoint_id}/pings", response_model=list[PingResultResponse])
 def get_all_the_ping_results(endpoint_id : int, db : Session = Depends(get_db)):
     return get_pings_of_endpoint(db, endpoint_id)
