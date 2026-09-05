e223c98012c6e1ebec74b17c453ae27867514afc
SwastikKaurav <kauravswastik15@gmail.com>
2026-09-06 00:15:19 +0530

    added getPings function

--- frontend/VigilAPI/src/api/endpoints.js
+++ frontend/VigilAPI/src/api/endpoints.js
@@ -8,3 +8,20 @@ export async function getEndpoints(){
         throw new Error(response.status)
     }
 }
+
+export async function getPings(endpoint_id){
+    let response = await fetch(`http://localhost:8000/endpoints/${endpoint_id}/pings`)
+    if(response.ok){
+        let data = await response.json();
+        return data;
+    }
+    else{
+        throw new Error(response.status)
+    }
+}
+
+async function pings(){
+    let data = await getPings()
+    console.log(data)
+}
+pings()
