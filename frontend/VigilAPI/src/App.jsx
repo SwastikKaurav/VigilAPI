f4785bb9fc3133e64831de08d1f6f740528ed209
SwastikKaurav <kauravswastik15@gmail.com>
2026-09-05 23:14:18 +0530

    added route for other paths

--- frontend/VigilAPI/src/App.jsx
+++ frontend/VigilAPI/src/App.jsx
@@ -1,12 +1,15 @@
 import { Route, Routes } from "react-router-dom"
 import Dashboard from "./Pages/Dashboard"
 import DetailPage from "./Pages/DetailPage"
+import NotFound from "./Pages/NotFound"
 
 export default function App(){
   return(
     <Routes>
       <Route path="/" element={<Dashboard/>}/>
       <Route path="/endpoints/:id" element={<DetailPage/>}/>
+      
+      <Route path="*" element={<NotFound/>}/>
     </Routes>
   )
 }
