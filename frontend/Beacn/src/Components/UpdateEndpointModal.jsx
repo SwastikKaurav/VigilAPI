0f90da0d2914217b673dcc4ad4423ff747683cd6
SwastikKaurav <kauravswastik15@gmail.com>
2026-09-09 01:03:12 +0530

    just a better ux for edit, instead of showing whole form evertime, just shows the edit button now.

--- frontend/Beacn/src/Components/UpdateEndpointModal.jsx
+++ frontend/Beacn/src/Components/UpdateEndpointModal.jsx
@@ -5,6 +5,7 @@ export default function UpdateEndpointModal({endpoint, onEndpointUpdated}){
     let [name, setName] = useState(endpoint.name);
     let [url, setUrl] = useState(endpoint.url);
     let [pingInterval, setPingInterval] = useState(endpoint.ping_interval);
+    let [isEditing, setIsEditing] = useState(false);
     
     function handleNameChange(e){
         setName(e.target.value);
@@ -21,12 +22,24 @@ export default function UpdateEndpointModal({endpoint, onEndpointUpdated}){
     async function handleSubmit(e){
         e.preventDefault();
         let data = {"name":name, "url":url, "ping_interval":parseInt(pingInterval)};
-        let response = await updateEndpoint(endpoint.id, data);
-        onEndpointUpdated(response);
+        try{
+            let response = await updateEndpoint(endpoint.id, data);
+            onEndpointUpdated(response);
+            setIsEditing(false);
+        }
+        catch (e) {
+            alert("Failed to update endpoint");
+        }
+        
+    }
+
+    function handleEdit(){
+        setIsEditing((edit)=>!edit);
     }
 
     return(
         <>
+            {isEditing ? 
             <form onSubmit={handleSubmit}>
                 <label>Name : </label>
                 <input value={name} onChange={handleNameChange}/>
@@ -38,7 +51,8 @@ export default function UpdateEndpointModal({endpoint, onEndpointUpdated}){
                 <input value={pingInterval} onChange={handlePingIntervalChange} type="number"/>
             
                 <button type="submit">Update</button>
-            </form>
+            </form> : <button onClick={handleEdit}>Edit</button>}
+            
         </>
     )
 }
